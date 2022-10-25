417f16d53484676ba086b9ba19d0dfadd6fdb3e3
Tasnim-Bahar-Samir <tanimsamir3@gmail.com>
2022-10-25 16:08:28 +0600

    added navbar

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,5 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
+import Blog from "../Pages/Blog/Blog";
+import Courses from "../Pages/Courses/Courses";
 import { Error } from "../Pages/Error/Error";
 import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
@@ -14,6 +16,14 @@ export const router = createBrowserRouter([
             {
                 path:'/',
                 element: <Home/>
+            },
+            {
+                path:'courses',
+                element:<Courses/>
+            },
+            {
+                path:'blog',
+                element:<Blog/>
             },
             {
                 path:'/login',
--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom';
+import logo from '../../assets/images/logo.webp'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="navbar bg-gray-200 px-8">
+        <div className="flex-1">
+            <Link to='/' className="normal-case text-3xl font-mono flex items-center">
+                <img className='text-primary w-10 rounded-sm' src={logo} alt="" />
+                <p>Code<span className=' text-orange-600'>Lab</span></p>
+            </Link>
+        </div>
+        <div className="flex gap-4 lg:gap-6 pr-5 text-lg">
+            <NavLink className='hover:text-orange-600' to='/courses'>Courses</NavLink>
+            <NavLink className='hover:text-orange-600' to='/blog'>FAQ</NavLink>
+            <NavLink className='hover:text-orange-600'  to='/courses'>Blog</NavLink>
+            <NavLink className='px-4 py-1 rounded text-white bg-orange-600' to='/blog'>Login</NavLink>
+        </div>
+    </div>
   )
 }
 
